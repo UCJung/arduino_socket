ddd1543a7ac5e78c02967b6f063e6a5f135ff0a5
mykumi <mykumi@gmail.com>
2015-03-31 20:10:24 +0900

    - Add SerialPort Module Dependency - add serial port listener - mod socket listener for serial port control

--- control/socket_module.js
+++ control/socket_module.js
@@ -1,17 +1,56 @@
-
 module.exports = function(server) {
 
 	var io = require('socket.io').listen(server);
-	 
-	io.sockets.on('connection', function(socket){
-		socket.on('requestData', function(msg){
-			io.emit('responseData', "ResponseData : " + msg);
-		});
-		
-		socket.on('requestControl', function(msg){
-			io.emit('responseControl', msg);
-		});	  
+	var SerialPort = require("serialport").SerialPort;
+	var serialPort = new SerialPort('COM1', {
+	    baudrate: 9600 
 	});	
+	console.log(serialPort);
+	socketListener(io, server, serialPort);
+	serialListener(io, serialPort);
 	
 	return io;
 } 
+
+function socketListener(io, server)
+{
+	io.sockets.on('connection', function(socket){
+		// process arduino request data message to serial port
+		socket.on('requestData', function(msg){
+			// serial write
+			serialPort.write(msg, function(err, result) {
+				console.log('err ' + err);
+				console.log('result ' + result);
+				io.emit('responseData', err + '/' + reuslt);
+			});
+		});
+		
+		// process arduino contorl request message to serial port
+		socket.on('requestControl', function(msg){
+			// serial write
+			serialPort.write(msg, function(err, result) {
+				console.log('err ' + err);
+				console.log('result ' + result);
+				io.emit('responseControl', err + '/' + reuslt);
+			});
+		});	  
+	});	
+}
+
+function serialListener(io, serialPort)
+{
+	var receivedData = "";
+	console.log(serialPort);
+	serialPort.on("open", function () {
+		// Listens to incoming data
+		serialPort.on('data', function(data) { 
+			receivedData += data.toString();
+			if (receivedData .indexOf('E') >= 0 && receivedData .indexOf('B') >= 0) {
+				sendData = receivedData .substring(receivedData .indexOf('B') + 1, receivedData .indexOf('E'));
+				receivedData = '';
+			}
+			console.log(11111);
+			io.emit('responseControl', sendData);
+		});  
+	});  
+}
